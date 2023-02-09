b3646d9e852c87e77bb8a884e59b42a0046327d9
Ihor Riabenko <100082550+iryabenkoff@users.noreply.github.com>
2023-02-09 20:37:40 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+Start
+1. npm i
+2. npm run dev
+
+Build for production
+1. npm run build
+
---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  context: path.resolve(__dirname, './src'),
+  entry: {
+    bundle: './index.js',
+  },
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: '[name][contenthash].js',
+    clean: true,
+    assetModuleFilename: '[name][ext]',
+  },
+  devtool: 'source-map',
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, './dist'),
+    },
+    port: 3000,
+    open: true,
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif|webp|avif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(ttf|woff|woff2|eot)$/,
+        use: ['file-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: './template.html',
+    }),
+  ],
+};
